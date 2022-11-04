beda93b9d59e7ba8abb81bfbe1b55655b6cc2414
wangzhang <745268436@qq.com>
2022-11-04 10:24:40 +0800

    ⚡️update 更新腾讯文档api

--- tencentdoc/excel.py
+++ tencentdoc/excel.py
@@ -9,6 +9,24 @@ def todate(day):
     real = theDay + timedelta(days=int(day))
     return real.strftime("%Y-%m-%d")
 
+class pb(object):
+    def __init__(self, s="", url=""):
+        self.s = s
+        self.url = url
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        if isinstance(obj, int):
+            return int(obj)
+        elif isinstance(obj, float):
+            return float(obj)
+        elif isinstance(obj, pb):
+            return {"url": obj.url, "str":obj.s }
+        else:
+            return super(MyEncoder, self).default(obj)
+    
 if __name__ == '__main__':
     cookie_data=download.user_data()
 
@@ -28,12 +46,11 @@ if __name__ == '__main__':
     for tab in tabs:
         tab_id = tab["id"]
         name = tab["name"]
-        obj[name] = []
+        obj[name] = []  # 存放json数据
         print("正在下载: %s" % name)
         sheet_content, max_col = download.read_sheet(url, tab_id, opendoc_params,cookie_data)
-        # print(sheet_content)
         row = []
-        row1 = []
+        row1 = [] # 存放json数据
         ws = wb.create_sheet(name)
         for k, v in sheet_content.items():
             if (int(k) % max_col == 0 and k != '0'):
@@ -44,21 +61,23 @@ if __name__ == '__main__':
                     row1 = []
                     fidx += 1
             if '2' in v:
+                tmp = pb()
                 if str(v['2'][1]).isdigit() and len(str(v['2'][1]))>4:
                     row.append(todate(int(v['2'][1])))
-                    if fidx < 2: row1.append(str(todate(int(v['2'][1]))))
+                    if fidx < 2: tmp.s = str(todate(int(v['2'][1])))
                 else:
                     row.append(v['2'][1])
-                    if fidx < 2: row1.append(str(v['2'][1]))
+                    if fidx < 2: tmp.s = str(v['2'][1])
                 if '6' in v:
-                    row.append(v['6'])
-                    if fidx<2 : row1.append(str(v['6']))
+                    # row.append(v['6']) # 表格不再保存链接数据
+                    if fidx<2 : tmp.url = str(v['6'])
+                if fidx<2: row1.append(tmp)
             else:
                 row.append("")
     empty_ws = wb["Sheet"]
     wb.remove(empty_ws)
     wb.save('%s.xlsx' % "tea")
     with open("tea.json", 'w') as file:
-        json.dump(obj, file)
+        json.dump(obj, file, cls=MyEncoder)
         print("save tea.json...")
     print("下载完成，已保存与根目录")
