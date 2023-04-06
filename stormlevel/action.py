88657554de5f2d03aa0c08b27612b7113aab10df
wangzhang <745268436@qq.com>
2023-04-06 17:55:39 +0800

    feat: stormlevel文件相关

---
+++ stormlevel/action.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import time
+import json
+
+downloadUrl = "https://raw.gitmirror.com/stormsunshine/LeetCode-Levels/main/LeetCode%20Levels.xlsx?timeStamp=" + str(int(time.time()))
+data = requests.get(downloadUrl)
+with open('./stormlevel/data.xlsx', 'wb') as f:
+    f.write(data.content)
+df = pd.read_excel(open("./stormlevel/data.xlsx", 'rb'), sheet_name='主站')
+data = df.values
+arr = []
+for d in data:
+    v = {}
+    v["ID"] = d[0]
+    v['TitleZH'] = d[1]
+    v["Url"] = d[2]
+    v["Ispaid"] = d[3]
+    v["Type"] = d[4]
+    v["Level"] = d[5]
+    v["NextLevel"] = d[6]
+    v["Difficulty"] = d[7]
+    if str(d[8]) == "nan": d[8] = ""
+    v["ContestInfo"] = str(d[8])
+    arr.append(v)
+with open("./stormlevel/data.json", 'w') as f:
+    f.write(json.dumps(arr))
