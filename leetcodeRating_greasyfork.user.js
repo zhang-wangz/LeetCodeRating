59748be4ba59c717d0a004df1dbac0281671dcfc
wangzhang <745268436@qq.com>
2025-02-13 11:07:12 +0800

    🐍 2.5.1 update: 由于油叉平台被gfw全面封锁，增加新的脚本下载平台<脚本猫>，去除了所有第三方脚本依赖，可以科学上网的的仍旧可以到油叉下载使用，没有的可以到脚本猫进行下载更新～

--- leetcodeRating_greasyfork.user.js
+++ leetcodeRating_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LeetCodeRating｜显示力扣周赛难度分
 // @namespace    https://github.com/zhang-wangz
-// @version      2.5.0
+// @version      2.5.1
 // @license      MIT
 // @description  LeetCodeRating 力扣周赛分数显现和相关力扣小功能，目前浏览器更新规则，使用该插件前请手动打开浏览器开发者模式再食用～
 // @author       小东是个阳光蛋(力扣名)
@@ -25,14 +25,13 @@
 // @connect      raw.githubusercontent.com
 // @require      https://unpkg.com/jquery@3.5.1/dist/jquery.min.js
 // @require      https://unpkg.com/layui@2.9.6/dist/layui.js
-// @require      https://scriptcat.org/lib/2779/1.2.1/NElementGetter.js
 // @grant        unsafeWindow
 // ==/UserScript==
 
 (async function () {
     'use strict';
 
-    let version = "2.5.0"
+    let version = "2.5.1"
     let pbstatusVersion = "version16"
     const dummySend = XMLHttpRequest.prototype.send;
     const originalOpen = XMLHttpRequest.prototype.open;
@@ -87,6 +86,165 @@
     // 是否使用动态布局
     let localVal = localStorage.getItem("used-dynamic-layout")
     let isDynamic = localVal != null ? localVal.includes("true") : false
+
+    // ElementGetter依赖相关
+    var ElementGetter = function() {
+        const _jQuery = Symbol('jQuery');
+        const _window = Symbol('window');
+        const _matches = Symbol('matches');
+        const _MutationObs = Symbol('MutationObs');
+        const _listeners = Symbol('listeners');
+        const _addObserver = Symbol('addObserver');
+        const _addFilter = Symbol('addFilter');
+        const _removeFilter = Symbol('removeFilter');
+        const _query = Symbol('query');
+        const _getOne = Symbol('getOne');
+        const _getList = Symbol('getList');
+        class ElementGetter {
+            [_addObserver](target, callback) {
+                const observer = new this[_MutationObs](mutations => {
+                    for (const mutation of mutations) {
+                        if (mutation.type === 'attributes') {
+                            callback(mutation.target);
+                            if (observer.canceled) return;
+                        }
+                        for (const node of mutation.addedNodes) {
+                            if (node instanceof Element) callback(node);
+                            if (observer.canceled) return;
+                        }
+                    }
+                });
+                observer.canceled = false;
+                observer.observe(target, {childList: true, subtree: true, attributes: true});
+                return () => {
+                    observer.canceled = true;
+                    observer.disconnect();
+                };
+            }
+            [_addFilter](target, filter) {
+                let listener = this[_listeners].get(target);
+                if (!listener) {
+                    listener = {
+                        filters: new Set(),
+                        remove: this[_addObserver](target, el => {
+                            listener.filters.forEach(f => f(el));
+                        })
+                    };
+                    this[_listeners].set(target, listener);
+                }
+                listener.filters.add(filter);
+            }
+            [_removeFilter](target, filter) {
+                const listener = this[_listeners].get(target);
+                if (!listener) return;
+                listener.filters.delete(filter);
+                if (!listener.filters.size) {
+                    listener.remove();
+                    this[_listeners].delete(target);
+                }
+            }
+            [_query](all, selector, parent, includeParent) {
+                const $ = this[_jQuery];
+                if ($) {
+                    let jNodes = includeParent ? $(parent) : $([]);
+                    jNodes = jNodes.add([...parent.querySelectorAll('*')]).filter(selector);
+                    if (all) {
+                        return $.map(jNodes, el => $(el));
+                    } else {
+                        return jNodes.length ? $(jNodes.get(0)) : null;
+                    }
+                } else {
+                    const checkParent = includeParent && this[_matches].call(parent, selector);
+                    if (all) {
+                        const result = checkParent ? [parent] : [];
+                        result.push(...parent.querySelectorAll(selector));
+                        return result;
+                    } else {
+                        return checkParent ? parent : parent.querySelector(selector);
+                    }
+                }
+            }
+            [_getOne](selector, parent, timeout) {
+                return new Promise(resolve => {
+                    const node = this[_query](false, selector, parent, false);
+                    if (node) return resolve(node);
+                    let timer;
+                    const filter = el => {
+                        const node = this[_query](false, selector, el, true);
+                        if (node) {
+                            this[_removeFilter](parent, filter);
+                            timer && clearTimeout(timer);
+                            resolve(node);
+                        }
+                    };
+                    this[_addFilter](parent, filter);
+                    if (timeout > 0) {
+                        timer = setTimeout(() => {
+                            this[_removeFilter](parent, filter);
+                            resolve(null);
+                        }, timeout);
+                    }
+                });
+            }
+            [_getList](selectorList, parent, timeout) {
+                return Promise.all(selectorList.map(selector => this[_getOne](selector, parent, timeout)));
+            }
+            constructor(jQuery) {
+                this[_jQuery] = jQuery && jQuery.fn && jQuery.fn.jquery ? jQuery : null;
+                this[_window] = window.unsafeWindow || document.defaultView || window;
+                const elProto = this[_window].Element.prototype;
+                this[_matches] = elProto.matches
+                    || elProto.matchesSelector
+                    || elProto.mozMatchesSelector
+                    || elProto.oMatchesSelector
+                    || elProto.webkitMatchesSelector;
+                this[_MutationObs] = this[_window].MutationObserver
+                    || this[_window].WebkitMutationObserver
+                    || this[_window].MozMutationObserver;
+                this[_listeners] = new WeakMap();
+            }
+            get(selector, ...args) {
+                const parent = typeof args[0] !== 'number' && args.shift() || this[_window].document;
+                const timeout = args[0] || 0;
+                if (Array.isArray(selector)) {
+                    return this[_getList](selector, parent, timeout);
+                } else {
+                    return this[_getOne](selector, parent, timeout);
+                }
+            }
+            each(selector, ...args) {
+                const parent = typeof args[0] !== 'function' && args.shift() || this[_window].document;
+                const callback = args[0];
+                const refs = new WeakSet();
+                const nodes = this[_query](true, selector, parent, false);
+                for (const node of nodes) {
+                    refs.add(this[_jQuery] ? node.get(0) : node);
+                    if (callback(node, false) === false) return;
+                }
+                const filter = el => {
+                    const nodes = this[_query](true, selector, el, true);
+                    for (const node of nodes) {
+                        const _el = this[_jQuery] ? node.get(0) : node;
+                        if (!refs.has(_el)) {
+                            refs.add(_el);
+                            if (callback(node, true) === false) {
+                                return this[_removeFilter](parent, filter);
+                            }
+                        }
+                    }
+                };
+                this[_addFilter](parent, filter);
+            }
+            create(domString, parent) {
+                const template = this[_window].document.createElement('template');
+                template.innerHTML = domString;
+                const node = template.content.firstElementChild || template.content.firstChild;
+                parent ? parent.appendChild(node) : node.remove();
+                return node;
+            }
+        }
+        return ElementGetter;
+    }();
 
     function getPbNameId(pbName) {
         pbName2Id = JSON.parse(GM_getValue("pbName2Id", "{}").toString())
