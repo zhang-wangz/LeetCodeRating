cbeff79c674f67ef8fd0cbb212b2cbb740aafb05
median-dxz <33348142+median-dxz@users.noreply.github.com>
2025-05-10 00:25:22 +0800

    update: 添加版本更新说明

--- leetcodeRating_greasyfork.user.js
+++ leetcodeRating_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LeetCodeRating｜显示力扣周赛难度分
 // @namespace    https://github.com/zhang-wangz
-// @version      3.0.1
+// @version      3.0.2
 // @license      MIT
 // @description  LeetCodeRating 力扣周赛分数显现和相关力扣小功能，目前浏览器更新规则，使用该插件前请手动打开浏览器开发者模式再食用～
 // @author       小东是个阳光蛋(力扣名)
